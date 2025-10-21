82182849cb88255ea1971e7655766260ff8f621f
EbamzD <eamzstd@gmail.com>
2025-10-21 14:37:08 -0600

    Hola Mundo
    Creación de Debug.Log

---
+++ Proyecto_1_EBAC/Assets/Scripts/HolaMundo.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Algo paso");
+        Debug.Log("Hola Mundo");
+        Debug.LogError("Error");
+        Debug.LogWarning("Advertencia");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
